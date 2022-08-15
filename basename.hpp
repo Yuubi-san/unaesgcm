fdc5d51afac8875b9355febb5d5318120f944e5f
mjk <yuubi-san@users.noreply.github.com>
2022-08-15 08:44:40 +0000

    unfsck `basename` on sane platforms

--- basename.hpp
+++ basename.hpp
@@ -7,8 +7,15 @@
 
 inline std::string_view basename( const std::string_view path ) noexcept
 {
+  static constexpr auto windows =
+#ifndef _WIN32
+    false
+#else
+    true
+#endif
+  ;
   const auto found = std::find_if( std::rbegin(path), std::rend(path),
-    [](const auto c){ return c == '/' or c == '\\'; } ).base();
+    [](const auto c){ return c == '/' or (windows and c == '\\'); } ).base();
   return { found, std::end(path) };
 }
 
